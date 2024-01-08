fa2085f601420497bdc47d651601a1b3daf2beab
Jack <projectbyjx@gmail.com>
2024-01-08 12:16:25 -0500

    + super impose auto-gen

--- analysis/video_super_impose.py
+++ analysis/video_super_impose.py
@@ -1,15 +1,25 @@
 ### Super Impose Video Frames:
 # %% -------------------------------- Import Lib -------------------------------- %% #
 import cv2
-import os
-from PIL import Image
 import numpy as np
 
-
+from matplotlib import pyplot as plt
 # %% ------------------------------- SUPER IMPOSE -------------------------------- %% #
 # ours:
 from utils.uwarl_util import create_all_folders
 
+def image_histogram_equalization(image, number_bins=256):
+    # from http://www.janeriksolem.net/histogram-equalization-with-python-and.html
+
+    # get image histogram
+    image_histogram, bins = np.histogram(image.flatten(), number_bins, density=True)
+    cdf = image_histogram.cumsum() # cumulative distribution function
+    cdf = (number_bins-1) * cdf / cdf[-1] # normalize
+
+    # use linear interpolation of cdf to find new pixel values
+    image_equalized = np.interp(image.flatten(), bins[:-1], cdf)
+
+    return image_equalized.reshape(image.shape), cdf
 class VideoSuperMan:
     _video_dir  :str = ""
     _output_dir :str = ""
@@ -25,76 +35,150 @@ class VideoSuperMan:
         create_all_folders(path)
         return path
         
-    def super_impose(self, filename, file_type=".mov"):
+    def super_impose(self, filename, if_skip_sampling, file_type=".mov"):
         intermediate_output_path_ = self._create_subfolder(filename)
         file_path_ = f"{self._video_dir}/{filename}{file_type}"
-        frame_count_  = self._sample_frames(file_path_, intermediate_output_path_)
+        frame_count_  = self._sample_frames(file_path_, intermediate_output_path_, if_skip_sampling=if_skip_sampling)
         out_image_ = self._impose_frames_from_directory(filename, frame_count_, intermediate_output_path_, self._output_dir)
         return out_image_
         
     #Extract frames from video into a folder
-    def _sample_frames(self, file_path, intermediate_folder, if_static_salient=False):
+    def _sample_frames(self, file_path, intermediate_folder, 
+        if_skip_sampling=False,
+        methods="MOG2", # methods in ["sub", "MOG2"]
+        if_floor_filter = False,
+        t_offset = 10,
+    ):
         vCap = cv2.VideoCapture(file_path)
         frame_count = 1
         tick = 0
         N_frames = vCap.get(cv2.CAP_PROP_FRAME_COUNT)
-        indices_ = range(0, int(N_frames), int(self._gap))
+        indices_ = list(range(t_offset, int(N_frames), int(self._gap)))
         N_samples = len(indices_)
         print("N_frames:",N_frames, " N_samples:", N_samples)
         
-        sa = None
+        if if_skip_sampling:
+            return N_samples
+            
+        # - apply initial empty bkg: 
+        path_bkg = f"{intermediate_folder}/../background_cropped.png"
+        frame_bkg = cv2.imread(path_bkg)
+        # - prep bkg subtraction:
+        if methods == "MOG2":
+            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
+            fgbg = cv2.createBackgroundSubtractorMOG2()
+            fgmask = fgbg.apply(frame_bkg)
+        else:
+            background_gray = cv2.cvtColor(frame_bkg, cv2.COLOR_BGR2GRAY)
+        if if_floor_filter:
+            lower_color = np.array([78, 5, 150])
+            upper_color = np.array([138, 8, 176])
+
+        mask_ = None
         for i in indices_:
             vCap.set(cv2.CAP_PROP_POS_FRAMES, i)
             ret,frame = vCap.read()
             if ret:
-                if if_static_salient:
-                    sa = cv2.saliency.StaticSaliencyFineGrained_create()
-                    (success, saliencyMap) = sa.computeSaliency(frame)
-                    print("success:", success)
-                    saliencyMap = (saliencyMap * 255).astype("uint8")
-                    threshMap = cv2.threshold(saliencyMap, 0, 255,cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-                    frame_rgba = cv2.cvtColor(frame, cv2.COLOR_RGB2RGBA)
-                    frame_rgba[:, :, 3] = threshMap
-                    
-                    print(f"Exporting frame {i}..")
-                    cv2.imwrite(f'{intermediate_folder}/frame_{frame_count}.png',frame_rgba)
-                    cv2.imwrite(f'{intermediate_folder}/saliency_{frame_count}.jpg',threshMap)
-                    frame_count += 1 
+                path_frame = f"{intermediate_folder}/frame_{frame_count}.png"
+                path_frame2 = f"{intermediate_folder}/de_frame_{frame_count}.png"
+                frame = frame[0:900, 340:1800]
+                cv2.imwrite(path_frame,frame)
+                frame_count += 1 
+                if methods == "MOG2":
+                    fgmask = fgbg.apply(frame)
+                    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
+                    cv2.imwrite(path_frame2, fgmask)
+                    mask_new_ = cv2.imread(path_frame2, cv2.IMREAD_UNCHANGED)
                 else:
-                    cv2.imwrite(f'{intermediate_folder}/frame_{frame_count}.png',frame)
-                    frame_count += 1 
+                    image_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                    diff = cv2.absdiff(background_gray, image_gray)
+                    threshold_value = 30
+                    _, mask_new_ = cv2.threshold(diff, threshold_value, 255, cv2.THRESH_BINARY)
+                if if_floor_filter:
+                    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+                    mask = cv2.inRange(hsv, lower_color, upper_color)
+                    plt.figure(); plt.imshow(mask);
+                    mask_inverse = cv2.bitwise_not(mask)
+                    mask_new_ = cv2.bitwise_and(mask_new_, mask_inverse)
+                # create cumulated masks:
+                if mask_ is not None:
+                    mask_ = mask_ + mask_new_
+                else:
+                    mask_ = mask_new_
             else:
                 break 
 
         vCap.release()
         cv2.destroyAllWindows()
+        # save cumulated masks:
+        if mask_ is not None:
+            path_frame = f"{intermediate_folder}/cumulated_de_frames.png"
+            cv2.imwrite(path_frame, mask_)
         return frame_count
 
-    def _impose_frames_from_directory(self, filename, frame_count, inter_folder, output_dir):
+    def _impose_frames_from_directory(self, 
+        filename, frame_count, 
+        inter_folder, output_dir,
+        t_start = 1,    
+    ):
         output_file_name_ = f"{output_dir}/super_{filename}.png"
         print(f"Super Imposing {frame_count} frames..")
-        alpha = 1.0
-        if frame_count > 1:
-            for i in range(1, frame_count):
+        img_base_ = None
+        if frame_count > 2:
+            for i in range(frame_count-1, t_start, -1):
                 print(">>> Overlapping frame:", i)
                 try:
                     img_new_ = cv2.imread(f"{inter_folder}/frame_{i}.png", cv2.IMREAD_UNCHANGED)
-                    if i > 1:
-                        # alpha_ = alpha_/2 + alpha_base
-                        # Alpha factor
-                        # rgb_channels = img_new_[:, :, 0:3]
-                        # alpha_channel = img_new_[:, :,  3]
-                        # alpha_factor = alpha_channel[:,:,np.newaxis].astype(np.float32) / 255.0
-                        # alpha_factor = np.concatenate((alpha_factor,alpha_factor,alpha_factor), axis=2)
-                        # base = rgb_channels.astype(np.float32) * alpha_factor
-                        img_base_ = img_base_ + 0.5 * img_new_
-                        alpha += 0.5
+                    mask_new_ = cv2.imread(f"{inter_folder}/de_frame_{i}.png", cv2.IMREAD_UNCHANGED)
+                    _, mask_new_ = cv2.threshold(mask_new_, 5, 255, cv2.THRESH_BINARY)
+
+                    print(img_new_.shape)
+                    print(mask_new_.shape)
+                    # img_new_[:,:,0], _ = image_histogram_equalization(img_new_[:,:,0])
+                    # img_new_[:,:,1], _ = image_histogram_equalization(img_new_[:,:,1])
+                    # img_new_[:,:,2], _ = image_histogram_equalization(img_new_[:,:,2])
+                    # background
+                    # img_background_ = cv2.imread(f"{inter_folder}/frame_{i}.png")
+                    # img_new_[:,:,:3] = img_new_[:,:,:3] + img_background_[:,:,:3] * 0.1
+                    # img_new_ = img_new_.astype(np.float32)/255.0
+                    if img_base_ is not None:
+                        # - apply mask:
+                        fg = cv2.bitwise_and(img_new_, img_new_, mask=mask_new_)
+                        plt.figure(); plt.imshow(fg);
+                        mask_inv_ = cv2.bitwise_not(mask_new_)
+                        bk = cv2.bitwise_and(img_base_, img_base_, mask=mask_inv_)
+                        plt.figure(); plt.imshow(bk);
+                        
+                        # combine foreground+background
+                        img_base_ = cv2.bitwise_or(fg, bk)
                     else:
-                        # base:
-                        img_base_ = img_new_
+                        img_base_ = img_new_  
+                        
+                    #     # base:
+                    #     img_base_ = cv2.imread(f"{inter_folder}/frame_1.png", cv2.IMREAD_UNCHANGED)
+                    #     img_base_ = cv2.cvtColor(img_base_, cv2.COLOR_RGB2RGBA)
+                    #     img_base_ = img_base_.astype(np.float32)/255.0
                 except:
                     raise("Error opening images! Abort!")
-            cv2.imwrite(output_file_name_, img_base_/alpha)
+            
+            # Blending:
+            img_base_0 = cv2.imread(f"{inter_folder}/frame_1.png")
+            print(np.max(img_base_0))
+            print(np.max(img_base_))
+            img_base_ = cv2.addWeighted(img_base_0, 0.5, img_base_, 0.5
+            , 0)
+            
+            # # histogram:
+            # if 0:
+            #     img_base_[:,:,0], _ = image_histogram_equalization(img_base_[:,:,0])
+            #     img_base_[:,:,1], _ = image_histogram_equalization(img_base_[:,:,1])
+            #     img_base_[:,:,2], _ = image_histogram_equalization(img_base_[:,:,2])
+            #     img_base_[:,:,3], _ = image_histogram_equalization(img_base_[:,:,3])
+            
+            plt.figure()
+            plt.imshow(img_base_)
+            # Next we stack our equalized channels back into a single image
+            cv2.imwrite(output_file_name_, img_base_)
         
         return output_file_name_
         
@@ -107,7 +191,7 @@ king_ = VideoSuperMan(
 )
 
 LIST_VIDEO_FILES = [
-    "Demo_1207_UD_OCC3",
+    # "Demo_1207_UD_OCC3",
     "Demo_1213_Clips_LR_FWD",
     "Demo_1213_Clips_LR_RVR",
     "Demo_1213_Clips_LR_SPI",
@@ -123,6 +207,14 @@ LIST_VIDEO_FILES = [
 ]
 for file in LIST_VIDEO_FILES:
     print(" === Processing:", file, " === ")
-    path = king_.super_impose(filename=file, file_type=".mp4")
+    path = king_.super_impose(filename=file, file_type=".mp4", if_skip_sampling=False)
     print(">>> Generated @", path)
 
+
+# %%
+
+# %%
+
+# %%
+
+# %%
